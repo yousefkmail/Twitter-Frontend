6752e667c5704db690904aa17764b79fb8eb8724
yousefkmail <yousef.hnaty@gmail.com>
2024-01-08 14:24:12 +0200

    edit

---
+++ src/Pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as Home } from "./Twitter/Home/Home";
+export { default as Profile } from "./Twitter/Profile/Profile";
+export { default as Tweet } from "./Twitter/Tweet/TweetPage";
+export { default as Search } from "./Twitter/Search/Search";
+export { default as HomeNoUser } from "./HomeNoUser";
